32be65664c70f125a1df51dd5fae11af2739ce2e
Venkata Bhattaram <tinitiate@users.noreply.github.com>
2020-02-14 21:08:25 -0500

    1

--- olap_sale_reports.sql
+++ olap_sale_reports.sql
@@ -13,3 +13,28 @@ from    dim_product p
        ,fact_sale   f
 where  p.product_id = f.product_id
 and    p.product_type in ('fruit', 'dairy','food', 'frozen food');
+
+
+-- 3. In which sale type teams should I invest
+select  count(1), st.type_name, p.product_type
+from    dim_product p
+       ,fact_sale      f
+       ,dim_sales_type st
+where  p.product_id = f.product_id 
+and    f.sales_type_id = st.sales_type_id
+group by st.type_name,p.product_type
+
+831	OFFLINE
+253	ONLINE
+-- Which season or group of months "dairy" has highest sales
+select  count(1), t.action_month, p.product_type
+from    dim_product p
+       ,fact_sale    f
+       ,dim_time    t
+where  p.product_id = f.product_id 
+and    f.time_id = t.time_id
+and    p.product_type = 'dairy'
+group by t.action_month,p.product_type
+order by t.action_month;
+
+
